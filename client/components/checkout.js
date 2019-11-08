467fa97982a8a80c25bb138013b8bda5e93f811e
nikkoe82 <nikkoelliott@gmail.com>
2019-11-08 05:30:11 -0600

    Add flex wrap styling 2 checkout address container

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -99,173 +99,183 @@ class unconnectedCheckout extends React.Component {
           <h1>Checkout</h1>
           {/* <h1>Checkout ({numberOfItems} items)</h1> */}
         </div>
-        <div id="shippingContainer">
-          <h3>Shipping Address:</h3>
-          <form onSubmit={() => this.handleShipSubmit(event)}>
-            {this.state.updateShip ? (
-              <div>
-                <label htmlFor="shipName">Name:</label>
-                <input
-                  name="shipName"
-                  value={this.state.shipName}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="shipStreet">Street:</label>
-                <input
-                  name="shipStreet"
-                  value={this.state.shipStreet}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="shipCity">City:</label>
-                <input
-                  name="shipCity"
-                  value={this.state.shipCity}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="shipState">State:</label>
-                <input
-                  name="shipState"
-                  value={this.state.shipState}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="shipZip">Zip Code:</label>
-                <input
-                  name="shipZip"
-                  value={this.state.shipZip}
-                  onChange={this.handleChange}
-                />
-              </div>
-            ) : (
-              <div>
-                <label htmlFor="shipName">Name:</label>
-                <input name="shipName" value={shipAddress.name} readOnly />
-                <br />
-                <label htmlFor="shipStreet">Street:</label>
-                <input name="shipStreet" value={shipAddress.street} readOnly />
-                <br />
-                <label htmlFor="shipCity">City:</label>
-                <input name="shipCity" value={shipAddress.city} readOnly />
-                <br />
-                <label htmlFor="shipState">State:</label>
-                <input name="shipState" value={shipAddress.state} readOnly />
-                <br />
-                <label htmlFor="shipZip">Zip Code:</label>
-                <input name="shipZip" value={shipAddress.zip} readOnly />
-              </div>
-            )}
-            <br />
-            {this.state.updateShip ? (
-              <button type="submit">Submit Changes</button>
-            ) : (
-              <button
-                type="button"
-                onClick={() =>
-                  this.handleShipClick(
-                    event,
-                    shipAddress.name,
-                    shipAddress.street,
-                    shipAddress.city,
-                    shipAddress.state,
-                    shipAddress.zip
-                  )
-                }
-              >
-                Update Shipping Address
-              </button>
-            )}
-          </form>
-          <div>
-            <input
-              type="checkbox"
-              className="addressCheckbox"
-              onChange={() => this.handleBillAddress(event)}
-            />
-            Billing and shipping address are the same.
+        <div className="addressContainer">
+          <div id="shippingContainer">
+            <h3>Shipping Address:</h3>
+            <form onSubmit={() => this.handleShipSubmit(event)}>
+              {this.state.updateShip ? (
+                <div>
+                  <label htmlFor="shipName">Name:</label>
+                  <input
+                    name="shipName"
+                    value={this.state.shipName}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="shipStreet">Street:</label>
+                  <input
+                    name="shipStreet"
+                    value={this.state.shipStreet}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="shipCity">City:</label>
+                  <input
+                    name="shipCity"
+                    value={this.state.shipCity}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="shipState">State:</label>
+                  <input
+                    name="shipState"
+                    value={this.state.shipState}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="shipZip">Zip Code:</label>
+                  <input
+                    name="shipZip"
+                    value={this.state.shipZip}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              ) : (
+                <div>
+                  <label htmlFor="shipName">Name:</label>
+                  <input name="shipName" value={shipAddress.name} readOnly />
+                  <br />
+                  <label htmlFor="shipStreet">Street:</label>
+                  <input
+                    name="shipStreet"
+                    value={shipAddress.street}
+                    readOnly
+                  />
+                  <br />
+                  <label htmlFor="shipCity">City:</label>
+                  <input name="shipCity" value={shipAddress.city} readOnly />
+                  <br />
+                  <label htmlFor="shipState">State:</label>
+                  <input name="shipState" value={shipAddress.state} readOnly />
+                  <br />
+                  <label htmlFor="shipZip">Zip Code:</label>
+                  <input name="shipZip" value={shipAddress.zip} readOnly />
+                </div>
+              )}
+              <br />
+              {this.state.updateShip ? (
+                <button type="submit">Submit Changes</button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={() =>
+                    this.handleShipClick(
+                      event,
+                      shipAddress.name,
+                      shipAddress.street,
+                      shipAddress.city,
+                      shipAddress.state,
+                      shipAddress.zip
+                    )
+                  }
+                >
+                  Update Shipping Address
+                </button>
+              )}
+            </form>
+            <div>
+              <input
+                type="checkbox"
+                className="addressCheckbox"
+                onChange={() => this.handleBillAddress(event)}
+              />
+              Billing and shipping address are the same.
+            </div>
           </div>
-        </div>
-        <div id="shippingContainer">
-          <h3>Billing Address:</h3>
-          <form onSubmit={() => this.handleBillSubmit(event)}>
-            {this.state.updateBill ? (
-              <div>
-                <label htmlFor="billName">Name:</label>
-                <input
-                  name="billName"
-                  value={this.state.billName}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="billStreet">Street:</label>
-                <input
-                  name="billStreet"
-                  value={this.state.billStreet}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="billCity">City:</label>
-                <input
-                  name="billCity"
-                  value={this.state.billCity}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="billState">State:</label>
-                <input
-                  name="billState"
-                  value={this.state.billState}
-                  onChange={this.handleChange}
-                />
-                <br />
-                <label htmlFor="billZip">Zip Code:</label>
-                <input
-                  name="billZip"
-                  value={this.state.billZip}
-                  onChange={this.handleChange}
-                />
-              </div>
-            ) : (
-              <div>
-                <label htmlFor="billName">Name:</label>
-                <input name="billName" value={billAddress.name} readOnly />
-                <br />
-                <label htmlFor="billStreet">Street:</label>
-                <input name="billStreet" value={billAddress.street} readOnly />
-                <br />
-                <label htmlFor="billCity">City:</label>
-                <input name="billCity" value={billAddress.city} readOnly />
-                <br />
-                <label htmlFor="billState">State:</label>
-                <input name="billState" value={billAddress.state} readOnly />
-                <br />
-                <label htmlFor="billZip">Zip Code:</label>
-                <input name="billZip" value={billAddress.zip} readOnly />
-              </div>
-            )}
-            <br />
-            {this.state.updateBill ? (
-              <button type="submit">Submit Changes</button>
-            ) : (
-              <button
-                type="button"
-                onClick={() =>
-                  this.handleBillClick(
-                    event,
-                    billAddress.name,
-                    billAddress.street,
-                    billAddress.city,
-                    billAddress.state,
-                    billAddress.zip
-                  )
-                }
-              >
-                Update Billing Address
-              </button>
-            )}
-          </form>
+          <div id="shippingContainer">
+            <h3>Billing Address:</h3>
+            <form onSubmit={() => this.handleBillSubmit(event)}>
+              {this.state.updateBill ? (
+                <div>
+                  <label htmlFor="billName">Name:</label>
+                  <input
+                    name="billName"
+                    value={this.state.billName}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="billStreet">Street:</label>
+                  <input
+                    name="billStreet"
+                    value={this.state.billStreet}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="billCity">City:</label>
+                  <input
+                    name="billCity"
+                    value={this.state.billCity}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="billState">State:</label>
+                  <input
+                    name="billState"
+                    value={this.state.billState}
+                    onChange={this.handleChange}
+                  />
+                  <br />
+                  <label htmlFor="billZip">Zip Code:</label>
+                  <input
+                    name="billZip"
+                    value={this.state.billZip}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              ) : (
+                <div>
+                  <label htmlFor="billName">Name:</label>
+                  <input name="billName" value={billAddress.name} readOnly />
+                  <br />
+                  <label htmlFor="billStreet">Street:</label>
+                  <input
+                    name="billStreet"
+                    value={billAddress.street}
+                    readOnly
+                  />
+                  <br />
+                  <label htmlFor="billCity">City:</label>
+                  <input name="billCity" value={billAddress.city} readOnly />
+                  <br />
+                  <label htmlFor="billState">State:</label>
+                  <input name="billState" value={billAddress.state} readOnly />
+                  <br />
+                  <label htmlFor="billZip">Zip Code:</label>
+                  <input name="billZip" value={billAddress.zip} readOnly />
+                </div>
+              )}
+              <br />
+              {this.state.updateBill ? (
+                <button type="submit">Submit Changes</button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={() =>
+                    this.handleBillClick(
+                      event,
+                      billAddress.name,
+                      billAddress.street,
+                      billAddress.city,
+                      billAddress.state,
+                      billAddress.zip
+                    )
+                  }
+                >
+                  Update Billing Address
+                </button>
+              )}
+            </form>
+          </div>
         </div>
         <div>
           <Cart />
