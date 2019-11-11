4da3939cc2465512af28cbf8773f637fb6db8c3e
Irob466 <34842116+Irob466@users.noreply.github.com>
2019-11-11 14:08:10 -0600

    Update index.js

--- client/store/index.js
+++ client/store/index.js
@@ -8,7 +8,6 @@ import cartReducer from './cart'
 import singleProductReducer from './singleProduct'
 import catalogReducer from './catalog'
 import addressReducer from './address'
-import {reducer as formReducer} from 'redux-form'
 
 const reducer = combineReducers({
   user: userReducer,
