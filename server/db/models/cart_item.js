2dc06c74981b7bb453e59fcf3d43db2f2bc5dea6
nikkoe82 <nikkoelliott@gmail.com>
2019-11-12 11:49:08 -0600

    Add seed and db changes

--- server/db/models/cart_item.js
+++ server/db/models/cart_item.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const CartItem = db.define('cart_item', {
-  quantity: Sequelize.STRING
+  quantity: Sequelize.STRING,
+  price: Sequelize.INTEGER,
+  orderId: Sequelize.INTEGER
 })
 
 module.exports = CartItem
