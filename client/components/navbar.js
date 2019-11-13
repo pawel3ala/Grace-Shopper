f00813f96479844221378ae2fb460726c18346b5
Ian Roberts <irob466@gmail.com>
2019-11-13 02:26:11 -0600

    still works

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -37,7 +37,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
             width={3}
             textAlign="right"
           >
-            <Link to="/catalog">
+            <Link to="/">
               <Icon name="th" size="big" />
             </Link>
             <Link to="/cart">
