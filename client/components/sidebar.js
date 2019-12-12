7661223593ef2d0f76eb058b5502378d9e8436ce
nikkoe82 <nikkoelliott@gmail.com>
2019-12-12 15:17:02 -0600

    Add correction to sidebar styling

--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -45,185 +45,200 @@ const CatalogSidebar = () => {
       Filter
     </Button>
   ) : (
-    <Sidebar
-      as={Segment}
-      animation="scale down"
-      direction="left"
-      visible={vis}
-      width="wide"
-    >
-      <form>
-        <Grid style={{marginBottom: -40, marginLeft: -40}}>
-          <Grid.Column floated="left">
-            <Button
-              icon
-              color="red"
-              onClick={() => {
-                event.preventDefault()
-                setVis(!vis)
-              }}
-            >
-              <Icon name="close" />
-            </Button>
-          </Grid.Column>
-        </Grid>
-        <Grid.Column width={12}>
-          <Input
-            icon={{name: 'search', circular: true}}
-            name="search"
-            value={search}
-            onChange={handleChange}
-            placeholder="Search"
-          />
-        </Grid.Column>
-        <Segment vertical>
-          <Grid>
-            <Grid.Column verticalAlign="middle" width={4}>
-              <Header as="h3">Categories</Header>
-            </Grid.Column>
-            <Grid.Column width={8}>
-              <List
-                animated
-                selection
-                divided
-                verticalAlign="middle"
-                floated="right"
+    <>
+      <Button
+        type="button"
+        color="blue"
+        style={{marginLeft: 10}}
+        onClick={() => {
+          event.preventDefault()
+          setVis(!vis)
+        }}
+      >
+        Filter
+      </Button>
+      <Sidebar
+        as={Segment}
+        animation="push"
+        direction="left"
+        visible={vis}
+        width="wide"
+      >
+        <form>
+          <Grid style={{marginBottom: -40, marginLeft: -40}}>
+            <Grid.Column floated="left">
+              <Button
+                icon
+                color="red"
+                onClick={() => {
+                  event.preventDefault()
+                  setVis(!vis)
+                }}
               >
-                {categories.map(({id, name}) => (
-                  <List.Item
-                    key={id}
-                    name="category"
-                    onClick={() => setQuery({page: 1, category: id}, 'pushIn')}
-                  >
-                    <List.Header>{name}</List.Header>
-                  </List.Item>
-                ))}
-                <Button
-                  type="button"
-                  onClick={() => setQuery({category: undefined}, 'pushIn')}
-                >
-                  Clear Filter
-                </Button>
-              </List>
+                <Icon name="close" />
+              </Button>
             </Grid.Column>
           </Grid>
-        </Segment>
-        <Segment vertical>
-          <Label attached="top left">Price</Label>
-          <Grid>
-            {[
-              {name: 'gte', label: 'Price', dir: 'Low'},
-              {name: 'lte', label: 'Price', dir: 'High'}
-            ].map(({name, label, dir}, i) => (
-              <Grid.Column key={name} width={6}>
-                <Input
-                  type="number"
-                  label={{
-                    content: dir
-                  }}
-                  labelPosition={i % 2 ? 'right' : 'left'}
-                  style={{width: '5rem'}}
-                  name={name}
-                  value={price && price[name] / 100}
-                  placeholder={label}
-                  onChange={({target: {value}}) =>
-                    setQuery(
-                      {
-                        page: 1,
-                        price: {
-                          ...price,
-                          [name]: value ? value * 100 : undefined
-                        }
-                      },
-                      'pushIn'
-                    )
+          <Grid.Column width={12}>
+            <Input
+              icon={{name: 'search', circular: true}}
+              name="search"
+              value={search}
+              onChange={handleChange}
+              placeholder="Search"
+            />
+          </Grid.Column>
+          <Segment vertical>
+            <Grid>
+              <Grid.Column verticalAlign="middle" width={4}>
+                <Header as="h3">Categories</Header>
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <List
+                  animated
+                  selection
+                  divided
+                  verticalAlign="middle"
+                  floated="right"
+                >
+                  {categories.map(({id, name}) => (
+                    <List.Item
+                      key={id}
+                      name="category"
+                      onClick={() =>
+                        setQuery({page: 1, category: id}, 'pushIn')
+                      }
+                    >
+                      <List.Header>{name}</List.Header>
+                    </List.Item>
+                  ))}
+                  <Button
+                    type="button"
+                    onClick={() => setQuery({category: undefined}, 'pushIn')}
+                  >
+                    Clear Filter
+                  </Button>
+                </List>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Segment vertical>
+            <Label attached="top left">Price</Label>
+            <Grid>
+              {[
+                {name: 'gte', label: 'Price', dir: 'Low'},
+                {name: 'lte', label: 'Price', dir: 'High'}
+              ].map(({name, label, dir}, i) => (
+                <Grid.Column key={name} width={6}>
+                  <Input
+                    type="number"
+                    label={{
+                      content: dir
+                    }}
+                    labelPosition={i % 2 ? 'right' : 'left'}
+                    style={{width: '5rem'}}
+                    name={name}
+                    value={price && price[name] / 100}
+                    placeholder={label}
+                    onChange={({target: {value}}) =>
+                      setQuery(
+                        {
+                          page: 1,
+                          price: {
+                            ...price,
+                            [name]: value ? value * 100 : undefined
+                          }
+                        },
+                        'pushIn'
+                      )
+                    }
+                  />
+                </Grid.Column>
+              ))}
+            </Grid>
+          </Segment>
+          <Segment vertical>
+            <Grid>
+              <Grid.Column width={6}>
+                <Segment>
+                  <Label attached="top left">Filter</Label>
+                  <select
+                    name="sortCol"
+                    value={sort.split('.')[0]}
+                    onChange={({target: {value}}) =>
+                      setQuery({sort: `${value}.${sort.split('.')[1]}`})
+                    }
+                  >
+                    <option name="id" value="id" />
+                    <option name="name" value="name">
+                      Name
+                    </option>
+                    <option name="price" value="price">
+                      Price
+                    </option>
+                  </select>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <Segment>
+                  <Label attached="top left">Direction</Label>
+                  <select
+                    name="sortCol"
+                    value={sort.split('.')[1]}
+                    onChange={({target: {value}}) =>
+                      setQuery({sort: `${sort.split('.')[0]}.${value}`})
+                    }
+                  >
+                    <option name="ASC" value="ASC">
+                      Low to High
+                    </option>
+                    <option name="DESC" value="DESC">
+                      High to Low
+                    </option>
+                  </select>
+                </Segment>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Grid centered>
+            <Grid.Column>
+              <Segment>
+                <Label ribbon>Average Rating</Label>
+                <Rating
+                  size="huge"
+                  name="review"
+                  maxRating={5}
+                  rating={(review && review.gte) || 0}
+                  icon="star"
+                  onRate={(_, {rating}) =>
+                    handleChange({
+                      target: {name: 'review', value: {gte: rating}}
+                    })
                   }
                 />
-              </Grid.Column>
-            ))}
-          </Grid>
-        </Segment>
-        <Segment vertical>
-          <Grid>
-            <Grid.Column width={6}>
-              <Segment>
-                <Label attached="top left">Filter</Label>
-                <select
-                  name="sortCol"
-                  value={sort.split('.')[0]}
-                  onChange={({target: {value}}) =>
-                    setQuery({sort: `${value}.${sort.split('.')[1]}`})
-                  }
-                >
-                  <option name="id" value="id" />
-                  <option name="name" value="name">
-                    Name
-                  </option>
-                  <option name="price" value="price">
-                    Price
-                  </option>
-                </select>
               </Segment>
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <Segment>
-                <Label attached="top left">Direction</Label>
-                <select
-                  name="sortCol"
-                  value={sort.split('.')[1]}
-                  onChange={({target: {value}}) =>
-                    setQuery({sort: `${sort.split('.')[0]}.${value}`})
-                  }
-                >
-                  <option name="ASC" value="ASC">
-                    Low to High
-                  </option>
-                  <option name="DESC" value="DESC">
-                    High to Low
-                  </option>
-                </select>
-              </Segment>
-            </Grid.Column>
-          </Grid>
-        </Segment>
-        <Grid centered>
-          <Grid.Column>
-            <Segment>
-              <Label ribbon>Average Rating</Label>
-              <Rating
-                size="huge"
-                name="review"
-                maxRating={5}
-                rating={(review && review.gte) || 0}
-                icon="star"
-                onRate={(_, {rating}) =>
-                  handleChange({
-                    target: {name: 'review', value: {gte: rating}}
-                  })
-                }
-              />
-            </Segment>
-            {/* <Button type="submit" floated="right">
+              {/* <Button type="submit" floated="right">
               Search
             </Button> */}
-          </Grid.Column>
-        </Grid>
-        <Grid style={{marginTop: -40}}>
-          <Grid.Column textAlign="right" floated="right">
-            <Button
-              icon
-              color="green"
-              onClick={() => {
-                event.preventDefault()
-                setVis(!vis)
-              }}
-            >
-              Done
-            </Button>
-          </Grid.Column>
-        </Grid>
-      </form>
-    </Sidebar>
+            </Grid.Column>
+          </Grid>
+          <Grid style={{marginTop: -40}}>
+            <Grid.Column textAlign="right" floated="right">
+              <Button
+                icon
+                color="green"
+                onClick={() => {
+                  event.preventDefault()
+                  setVis(!vis)
+                }}
+              >
+                Done
+              </Button>
+            </Grid.Column>
+          </Grid>
+        </form>
+      </Sidebar>
+    </>
   )
 }
 
