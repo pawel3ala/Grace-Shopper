db285fbbd7365fa1e18153e3a0bb79e8fc40a6db
Pawel Bala <pawel3ala@gmail.com>
2020-02-29 15:17:18 -0600

    getting rid of hardcoded credentials

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,40 +1,55 @@
 describe('Login', () => {
-    beforeEach(function () {
-        cy.visit('/')
-        cy.get('a[href="/login"]').click()
+  beforeEach(function() {
+    cy.visit('/')
+    cy.get('a[href="/login"]').click()
+  })
+
+  it('displays errors on login', () => {
+    cy.fixture('credentials').then(credentials => {
+      const {email} = credentials
+      const {password} = credentials
+
+      cy.get('input[name=email]').type(email)
+      cy.get('input[name=password]').type(`${password}typo{enter}`)
     })
 
-    it('displays errors on login', () => {
-        cy.get('input[name=email]').type('dupa@ppp.com')
-        cy.get('input[name=password]').type('password123{enter}')
+    cy
+      .get('form div')
+      .should('be.visible')
+      .and('contain', 'Wrong username and/or password')
 
-        cy
-            .get('form div')
-            .should('be.visible')
-            .and('contain', 'Wrong username and/or password')
+    cy.url().should('include', '/login')
+  })
 
-        cy.url().should('include', '/login')
+  it('user is able to log in', () => {
+    cy.fixture('credentials').then(credentials => {
+      const {email} = credentials
+      const {password} = credentials
+
+      cy.get('input[name=email]').type(email)
+      cy.get('input[name=password]').type(`${password}{enter}`)
     })
 
-    it('user is able to log in', () => {
-        cy.get('input[name=email]').type('pawel@fsa.com')
-        cy.get('input[name=password]').type('pawel{enter}')
+    cy.get('h3').should('contain', 'Welcome, pawel@fsa.com')
 
-        cy.get('h3').should('contain', 'Welcome, pawel@fsa.com')
+    cy.url().should('include', '/home')
+    cy.get('a[href="#"]').should('contain', 'Logout')
+    cy.get('a[href="/order"]').should('contain', 'Orders')
+  })
 
-        cy.url().should('include', '/home')
-        cy.get('a[href="#"]').should('contain', 'Logout')
-        cy.get('a[href="/order"]').should('contain', 'Orders')
+  it('able to succesfully log out', () => {
+    cy.fixture('credentials').then(credentials => {
+      const {email} = credentials
+      const {password} = credentials
+
+      cy.get('input[name=email]').type(email)
+      cy.get('input[name=password]').type(`${password}{enter}`)
     })
 
-    it('able to succesfully log out', () => {
-        cy.get('input[name=email]').type('pawel@fsa.com')
-        cy.get('input[name=password]').type('pawel{enter}')
+    cy.get('h3').should('contain', 'Welcome, pawel@fsa.com')
 
-        cy.get('h3').should('contain', 'Welcome, pawel@fsa.com')
-
-        cy.url().should('include', '/home')
-        cy.get('a[href="#"]').click()
-        cy.get('a[href="/login"]').should('contain', 'Login')
-    })
+    cy.url().should('include', '/home')
+    cy.get('a[href="#"]').click()
+    cy.get('a[href="/login"]').should('contain', 'Login')
+  })
 })
